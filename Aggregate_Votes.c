0145721858d8ea4c7aff739c927c39b0ec6516f4
Travis Slettvedt <slett142@umn.edu>
2018-03-06 15:16:24 -0600

    Worked on AV

--- Aggregate_Votes.c
+++ Aggregate_Votes.c
@@ -1,4 +1,5 @@
 #define NUM_ARGS 1
+#define MAX_CANDIDATES 100
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,7 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <string.h>
+#include "makeargv.h"
 
 int isLeaf(char* path);
 void aggregateVotes(char* path);
@@ -49,9 +51,15 @@ output: Aggregation results in "<path>.txt" files and writes the output
   file path to stdout
 ************************************************************************/
 void aggregateVotes(char* path){
-  if(isLeaf(path)){
-
+  int candidateVotes[MAX_CANDIDATES];
+  int numberOfCandidates = 0;
+  char* candidateNames[MAX_CANDIDATES][2];
+  int i;
+  for(i=0; i<MAX_CANDIDATES; i++){
+    candidateVotes[i] = 0;
+    // candidateNames[i] = malloc(1024);
   }
+  char* subresultsfile, newresultsfile;
 
   DIR* curdir = opendir(path);
   struct dirent* subdir;
@@ -67,18 +75,73 @@ void aggregateVotes(char* path){
         pid_t pid = fork();
         if(pid == 0){
           char* newpath = malloc(strlen(path)+strlen(subdir->d_name)+10);
-          sprintf(newpath, "%s%s/", path, subdir->d_name);
-          execl("./Aggregate_Votes", "Aggregate_Votes", newpath, (char*)NULL);
-          perror("Exec failed.\n");
+          sprintf(newpath, "%s%s", path, subdir->d_name);
+          if(isLeaf(newpath)){
+            printf("newpath: %s\n", newpath);
+            execl("./Leaf_Counter", "Leaf_Counter", newpath, (char*)NULL);
+            perror("Exec Leaf_Counter failed.\n");
+          }
+          else {
+            printf("newpath: %s\n", newpath);
+            execl("./Aggregate_Votes", "Aggregate_Votes", newpath, (char*)NULL);
+            perror("Exec Aggregate_Votes failed.\n");
+          }
         }
         else {
           wait(NULL);
-          
-
+          sprintf(subresultsfile, "%s%s.txt", path, subdir->d_name);
+          path[strlen(path)-1] = 0;
+          sprintf(newresultsfile, "%s.txt", path);
+          FILE* subresults = fopen(subresultsfile, "r");
+          if(subresults == NULL){
+            printf("error opening file %s\n", subresultsfile);
+            exit(0);
+          }
+          char* line;
+          size_t len = 0;
+          while(getline(&line, &len, subresults)!= -1){
+            trimwhitespace(line);
+            char** candidateArray;
+            int n = makeargv(line, ",", candidateArray);
+            if(numberOfCandidates == 0)
+              numberOfCandidates = n;
+            int j,k;
+            for(j=0; j<n; j++){
+              char** temp;
+              makeargv(candidateArray[k], ":", temp);
+              printf("temp[0]: %s, temp[1]: %s", temp[0], temp[1]);
+              for(k=0; k<MAX_CANDIDATES; k++){
+                if(candidateVotes[k]>0 && strcmp(candidateNames[k],temp[0])==0){ //if they have the same name
+                  candidateVotes[k] += atoi(temp[1]); //add a vote to that candidate
+                  break;
+                }
+                else if(candidateVotes[k]==0){//if we find an empty index in array then we have a new candidate
+                  candidateVotes[k] += atoi(temp[1]);
+                  strcpy(candidateNames[k],temp[0]);
+                  break;
+                }
+              }
+            }
+          }
+          if(line)
+            free(line);
         }
       }
     }
   }
+  // print to output file
+  FILE* newresults = fopen(newresultsfile, "w");
+  if(newresults == NULL){
+    printf("error opening file %s\n", newresultsfile);
+    exit(0);
+  }
+  char* output;
+  int l;
+  for(l=0; l<numberOfCandidates-1; l++){
+    sprintf(output, "%s:%d,", candidateNames[l], candidateVotes[l]);
+  }
+  sprintf(output, "%s:%d\n", candidateNames[numberOfCandidates-1], candidateVotes[numberOfCandidates-1]);
+  fputs(output, newresults);
 }
 
 int main(int argc, char** argv)
